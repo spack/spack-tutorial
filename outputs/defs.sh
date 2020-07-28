b55e6ee37666ffdc7bc8e4c50e285ca1bb067fb1
Greg Becker <becker33@llnl.gov>
2020-07-27 20:09:45 -0700

    Update and automate dev workflow section (#56)
    * update dev section

--- outputs/defs.sh
+++ outputs/defs.sh
@@ -19,6 +19,33 @@ example() {
         cmd="spack env deactivate"
     fi
 
+    echo $cmd
+    # get the command's output
+    $cmd | tee -a "$filename"
+}
+
+# This allows us to echo a different command than we run
+# Used to stay out of the build environment subshell
+fake_example(){
+    filename="$raw_outputs/$1.out"
+    shift
+
+    fake_cmd="$1"
+    shift
+
+    parent=$(dirname $filename)
+    mkdir -p $parent
+
+    # print the command to the file
+    echo "$ $fake_cmd" &>> "$filename"
+
+    cmd="$@"
+
+    # Don't use despacktivate alias (and strip comment)
+    if [[ "$cmd" == despacktivate* ]]; then
+        cmd="spack env deactivate"
+    fi
+
     # get the command's output
     $cmd | tee -a "$filename"
 }
