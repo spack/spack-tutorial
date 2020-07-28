ec7dda80b1ce70956e0941daab5f2266aaceb857
Gregory Becker <becker33@llnl.gov>
2020-07-27 19:25:39 -0700

    update dev section

--- outputs/defs.sh
+++ outputs/defs.sh
@@ -19,6 +19,31 @@ example() {
         cmd="spack env deactivate"
     fi
 
+    echo $cmd
+    # get the command's output
+    $cmd | tee -a "$filename"
+}
+
+fake_example(){
+    filename="$raw_outputs/$1.out"
+    shift
+
+    fake_cmd="$1"
+    shift
+
+    parent=$(dirname $filename)
+    mkdir -p $parent
+
+    # print the command to the file
+    echo "$ $fake_cmd" &>> "$filename"
+
+    cmd="$@"
+
+    # Don't use despacktivate alias (and strip comment)
+    if [[ "$cmd" == despacktivate* ]]; then
+        cmd="spack env deactivate"
+    fi
+
     # get the command's output
     $cmd | tee -a "$filename"
 }
