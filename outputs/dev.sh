49e31e5cd8ea40a1b885a09d1b37bdb692341fe4
Greg Becker <becker33@llnl.gov>
2024-11-18 05:35:03 -0800

    update outputs (#415)
    * update outputs
    
    * remove appending issues

--- outputs/dev.sh
+++ outputs/dev.sh
@@ -54,34 +54,34 @@ example dev/otherdevel "ls"
 
 example dev/wrapup "spack undevelop scr macsio"
 
-example dev/optional-intro "spack develop --help"
+# example dev/optional-intro "spack develop --help"
 
-example dev/setting-src-path '# pre-clone the source code and then point spack develop to it'
-example dev/setting-src-path '# note that we can clone into any repo/branch combination desired'
-example dev/setting-src-path 'git clone https://github.com/llnl/scr.git $SPACK_ENV/code'
-example dev/setting-src-path '# note that with `--path` the code directory and package name can be different'
-example dev/setting-src-path 'spack develop --path $SPACK_ENV/code scr@3.1.0'
-example dev/setting-src-path 'spack concretize -f'
+# example dev/setting-src-path '# pre-clone the source code and then point spack develop to it'
+# example dev/setting-src-path '# note that we can clone into any repo/branch combination desired'
+# example dev/setting-src-path 'git clone https://github.com/llnl/scr.git $SPACK_ENV/code'
+# example dev/setting-src-path '# note that with `--path` the code directory and package name can be different'
+# example dev/setting-src-path 'spack develop --path $SPACK_ENV/code scr@3.1.0'
+# example dev/setting-src-path 'spack concretize -f'
 
-example dev/navigation-and-build-env "spack build-env scr -- bash"
-example dev/navigation-and-build-env "# Shell wrappers didn't propagate to the subshell"
-example dev/navigation-and-build-env "source $SPACK_ROOT/share/spack/setup-env.sh"
-example dev/navigation-and-build-env "# Let's look at navigation features"
-example dev/navigation-and-build-env "spack cd --help"
-example dev/navigation-and-build-env "spack cd -c scr"
-example dev/navigation-and-build-env "touch src/scr_copy.c"
-example dev/navigation-and-build-env "spack cd -b scr"
-example dev/navigation-and-build-env "# Let's look at what's here"
-example dev/navigation-and-build-env "ls"
-example dev/navigation-and-build-env "# Build and run tests"
-example dev/navigation-and-build-env "make -j2"
-example dev/navigation-and-build-env "make test"
-example dev/navigation-and-build-env "exit"
+# fake_example dev/navigation-and-build-env "spack build-env scr -- bash" "/bin/true"
+# example dev/navigation-and-build-env "# Shell wrappers didn't propagate to the subshell"
+# example dev/navigation-and-build-env "source $SPACK_ROOT/share/spack/setup-env.sh"
+# example dev/navigation-and-build-env "# Let's look at navigation features"
+# example dev/navigation-and-build-env "spack cd --help"
+# example dev/navigation-and-build-env "spack cd -c scr"
+# fake_example dev/navigation-and-build-env "touch src/scr_copy.c" "/bin/true"
+# fake_example dev/navigation-and-build-env "spack cd -b scr" "/bin/true"
+# example dev/navigation-and-build-env "# Let's look at what's here"
+# example dev/navigation-and-build-env "ls"
+# example dev/navigation-and-build-env "# Build and run tests"
+# fake_example dev/navigation-and-build-env "make -j2" "/bin/true"
+# fake_example dev/navigation-and-build-env "make test" "/bin/true"
+# fake_example dev/navigation-and-build-env "exit" "/bin/true"
 
-example dev/combinatorics "# First we have to allow repeat specs in the environment"
-example dev/combinatorics "spack config add concretizer:unify:false"
-example dev/combinatorics "# Next we need to specify the specs we want ('==' propagates the variant to deps)"
-example dev/combinatorics "spack change macsio build_type==Release"
-example dev/combinatorics "spack add macsio+scr build_type==Debug"
-example dev/combinatorics "# Inspect the graph for multiple dev_path="
-example dev/combinatorics "spack concretize -f"
+# example dev/combinatorics "# First we have to allow repeat specs in the environment"
+# example dev/combinatorics "spack config add concretizer:unify:false"
+# example dev/combinatorics "# Next we need to specify the specs we want ('==' propagates the variant to deps)"
+# example dev/combinatorics "spack change macsio build_type==Release"
+# example dev/combinatorics "spack add macsio+scr build_type==Debug"
+# example dev/combinatorics "# Inspect the graph for multiple dev_path="
+# example dev/combinatorics "spack concretize -f"
