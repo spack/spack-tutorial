b54a5eab85ebcaff0af989cd64d24a08ba06a563
Caetano Melone <melone1@llnl.gov>
2025-06-03 11:18:50 -0700

    Update of license headers
    Changing the *rst files for now, the rest of the outdated notices are in
    package.py files that will be updated soon.

--- conf.py
+++ conf.py
@@ -1,5 +1,4 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
@@ -83,7 +82,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Spack Tutorial'
-copyright = u'2013-2024, Lawrence Livermore National Laboratory.'
+copyright = u'Spack Project Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
