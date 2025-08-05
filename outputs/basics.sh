6f9fcde73aaf08387ed0f52f68ba6f0532a56dc6
Alec Scott <scott112@llnl.gov>
2025-08-04 19:45:22 -0700

    Drop compilers from hdf5 spec

--- outputs/basics.sh
+++ outputs/basics.sh
@@ -62,7 +62,7 @@ example basics/hdf5-spec       "spack spec hdf5"
 example basics/hdf5            "spack install hdf5"
 example basics/hdf5-no-mpi     "spack install hdf5~mpi"
 
-example basics/hdf5-hl-mpi     "spack install hdf5+hl+mpi %c,cxx=gcc ^mpich"
+example basics/hdf5-hl-mpi     "spack install hdf5+hl+mpi ^mpich"
 
 example basics/find-ldf-2      "spack find -ldf"
 
