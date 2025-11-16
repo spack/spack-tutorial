a1e2cf56881e3501aca31bda85f14225477b959d
Alec Scott <scott112@llnl.gov>
2025-11-15 17:56:01 -0700

    Back off py-numpy entirely due to concretization failure

--- outputs/environments.sh
+++ outputs/environments.sh
@@ -108,8 +108,11 @@ example environments/incremental-1 "spack install --add python"
 # seemingly indefinitely. (Have tested waiting 1h 15m before giving up.)
 # example environments/incremental-1 "spack install --add py-numpy@1.20 2>&1 | tail -n1"
 
-example environments/incremental-2 "spack add py-numpy@1.20"
-example environments/incremental-2 "spack concretize -f"
+# v1.1: Spack failed to concretize the environment after adding py-numpy
+# https://github.com/spack/spack-tutorial/actions/runs/19397756937/job/55500422438?pr=501
+# example environments/incremental-2 "spack add py-numpy@1.20"
+# example environments/incremental-2 "spack concretize -f"
+
 spack env deactivate
 
 example --tee environments/show-mpicc-1      "spack env activate myproject2"
