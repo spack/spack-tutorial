ba492498c4ffdd5f62f665c7b8716cd49d5e0b88
Alec Scott <alec@llnl.gov>
2026-06-19 11:21:24 -0700

    Re-enable a minimal output generation as base for other PRs
    Signed-off-by: Alec Scott <alec@llnl.gov>

--- outputs/defs.sh
+++ outputs/defs.sh
@@ -14,8 +14,8 @@ fi
 raw_outputs="${PROJECT}/raw"
 
 # used by scripts
-tutorial_branch=releases/v1.1
-packages_release=v2025.11.0
+tutorial_branch=releases/v1.2
+packages_release=releases/v2026.06
 
 print_status() {
     printf "\n%b: %s\n\n" "\033[1;35m$1\033[0m" "$2"
